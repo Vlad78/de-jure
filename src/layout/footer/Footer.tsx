31e7eb10f099372a891392b7aac705b8382e68db
Vlad Razvenkov <dssdom@gmail.com>
2024-03-17 16:29:18 +0100

    feat: footer mobile adaptive

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -30,12 +30,14 @@ export const Footer = () => {
         <FlexWrapper direction="column" align="center" margin="73px 105px">
           <IconStripe iconId="logo" width="50px" fill={theme.colors.fontShaddy} />
           <div className="company">{creds.company}</div>
+
           <h4>{t("general information")}</h4>
-          <FlexWrapper margin="40px 0 0 0" gap="40px">
+          <FlexWrapper margin="40px 0 0 0" gap="40px" className="flex-1">
             <Menu icons="gray" />
           </FlexWrapper>
+
           <h4>{t("we are in messengers")}</h4>
-          <FlexWrapper margin="30px 0 0 0" gap="63px">
+          <FlexWrapper margin="30px 0 0 0" gap="63px" className="flex-2">
             <a href={creds.tg}>
               <IconStripe iconId="tg" height="30px" width="30px" />
             </a>
@@ -46,6 +48,7 @@ export const Footer = () => {
           <div className="business-hours">
             {t("we are available")} {tSys("working hours")}
           </div>
+
           <h4>{t("interested in a job?")}</h4>
           <div className="job">
             <a href={creds.linkedin}>
@@ -76,6 +79,18 @@ export const Footer = () => {
   );
 };
 
+const StyledUl = styled.ul`
+  margin-top: 60px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-auto-flow: column;
+  row-gap: 35px;
+  column-gap: 35px;
+  justify-items: start;
+  text-align: left;
+`;
+
 const StyledFooter = styled.footer`
   margin-top: 200px;
   min-height: 500px;
@@ -87,6 +102,7 @@ const StyledFooter = styled.footer`
 
   .company {
     margin-top: 10px;
+    white-space: nowrap;
   }
 
   h4 {
@@ -99,7 +115,7 @@ const StyledFooter = styled.footer`
 
   .business-hours {
     margin-top: 80px;
-    width: 380px;
+    max-width: 380px;
     font-size: ${font(16, 24)};
     font-weight: 400;
     line-height: 150%;
@@ -128,15 +144,74 @@ const StyledFooter = styled.footer`
   .design {
     margin-top: 100px;
   }
-`;
 
-const StyledUl = styled.ul`
-  margin-top: 60px;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(3, 1fr);
-  grid-auto-flow: column;
-  row-gap: 35px;
-  column-gap: 35px;
-  justify-items: start;
+  @media ${theme.media.tablet} {
+    ${Container} > ${FlexWrapper}:nth-child(1) {
+      margin: 73px 85px;
+      .flex-1 {
+        margin-top: 46px;
+        flex-direction: column;
+        gap: 40px;
+
+        ${FlexWrapper} {
+          justify-content: center;
+        }
+      }
+    }
+  }
+
+  @media ${theme.media.mobile} {
+    margin-top: 100px;
+
+    ${Container} > ${FlexWrapper}:nth-child(1) {
+      margin: 42px 30px;
+
+      h4 {
+        font-size: 20px;
+        margin-top: 50px;
+      }
+
+      .flex-1 {
+        margin-top: 46px;
+        flex-direction: column;
+        gap: 40px;
+
+        ${FlexWrapper} {
+          justify-content: center;
+          div {
+            white-space: wrap;
+          }
+        }
+      }
+
+      .business-hours {
+        margin-top: 50px;
+        font-size: 20px;
+      }
+
+      .job {
+        margin-top: 46px;
+      }
+
+      hr {
+        margin-top: 44.5px;
+        width: 100vw;
+      }
+
+      ${StyledUl} {
+        min-height: unset;
+        margin-top: 37px;
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-rows: 1fr;
+        grid-auto-flow: row;
+        align-items: center;
+        column-gap: 68px;
+        margin-bottom: 20px;
+      }
+
+      .terms {
+        margin-top: 19px;
+      }
+    }
+  }
 `;
